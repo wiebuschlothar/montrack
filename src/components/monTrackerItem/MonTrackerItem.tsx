1ce380c363dae8dba092aa79190fb9a35b5da6b5
wiebusch.lothar@gmail.com <wiebusch.lothar@gmail.com>
2022-10-20 18:57:41 +0200

    Added MonTrackerItem and List

---
+++ src/components/monTrackerItem/MonTrackerItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const MonTrackerItem = (props: Props) => {
+  return <div>MonTrackerItem</div>;
+};
+
+export default MonTrackerItem;
---
+++ src/components/monTrackerList/MonTrackerList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MonTrackerItem from "../monTrackerItem/MonTrackerItem";
+
+type Props = {};
+
+const MonTrackerList = (props: Props) => {
+  return (
+    <div>
+      <MonTrackerItem />
+    </div>
+  );
+};
+export default MonTrackerList;
